05283d28eb228e7c9efbbfa556215520da822a22
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-02-17 03:48:18 +0900

    [FEAT] 개인 정보 수정 구현 완료
    - 회원 정보 완료
    - 비밀번호 완료
    - 수정 완료 안내 버튼 로직 구현 안 함
    - 팝업 남음
    - 전반적인 디테일 봐야 함
    - 기타 애니메이션 자세한 스토리 보드 보기

---
+++ data/user.js
@@ -0,0 +1,9 @@
+export const users = [
+    {
+        id : 1,
+        name : "스타트업코드",
+        email: "startupcode@gmail.com",
+        profileImg : "../assets/img/defaultProfile.png",
+        password : "tmxkxmdjqzhem",
+    },
+];
