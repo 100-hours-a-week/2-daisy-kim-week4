af3996baad07fc58851a8a6a90571c5cef87f2e6
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-02-23 19:20:43 +0900

    [feat] events 추가
    - header 아무말 대잔치 클릭 시 게시글 목록 화면으로 가도록 js 작성
    -> 로그인/회원가입 화면 제외
    - 내 게시글, 내 댓글일 때만 수정, 삭제 버튼 뜨도록 수정
    - 수정 버튼 누르면 textarea에 반영되도록 수정

---
+++ events/header.js
@@ -0,0 +1,5 @@
+const goMain = document.getElementById('header');
+const goMainAction = goMain.addEventListener('click', () => {
+  window.location.href = '../board/boardList.html';
+});
+export default goMainAction;
